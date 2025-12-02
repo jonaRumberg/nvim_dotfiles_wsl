e94ba218c2766a62a25e0236405c23dfefe18e4d
Jona Rumberg <jona.rumberg@sap.com>
2025-12-02 08:45:05 +0100

    jump to hunk keymap

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,7 +2,36 @@ return {
     {
         "lewis6991/gitsigns.nvim",
         event = "BufEnter",
-        opts = {},
+        opts = {
+            on_attach = function(bufnr)
+                local gitsigns = require('gitsigns')
+
+                local function map(mode, l, r, opts)
+                    opts = opts or {}
+                    opts.buffer = bufnr
+                    vim.keymap.set(mode, l, r, opts)
+                end
+
+                -- Navigation
+                map('n', 'ü', function()
+                    if vim.wo.diff then
+                        vim.cmd.normal({']c', bang = true})
+                    else
+                        gitsigns.nav_hunk('next')
+                    end
+                end)
+
+                map('n', 'Ü', function()
+                    if vim.wo.diff then
+                        vim.cmd.normal({']c', bang = true})
+                    else
+                        gitsigns.nav_hunk('next')
+                    end
+                end)
+
+            end
+
+        },
     },
     {
         'pwntester/octo.nvim',
